94614525230b3810cce0bfa77b4a183d516b90df
최영후 <dudgn6392@naver.com>
2023-05-11 15:03:54 +0900

    Multiplay Project Setup

---
+++ Source/Multiplay_TPS/Multiplay_TPS.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Multiplay_TPS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Multiplay_TPS, "Multiplay_TPS" );
