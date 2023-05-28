8e81668ae971a413a2a5a968a74eabc785476b33
최영후 <dudgn6392@naver.com>
2023-05-28 18:47:22 +0900

    Seamless Travel

---
+++ Source/Multiplay_TPS/LobbyGameMode.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LobbyGameMode.h"
+#include "GameFramework/GameStateBase.h"
+
+void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+
+	int32 numberOfPlayers = GameState.Get()->PlayerArray.Num();
+	if (numberOfPlayers == 2)
+	{
+		UWorld* world = GetWorld();
+		if (world)
+		{
+			bUseSeamlessTravel = true;
+			world->ServerTravel(FString("/Game/Maps/TPSMap?listen"));
+		}
+	}
+}
