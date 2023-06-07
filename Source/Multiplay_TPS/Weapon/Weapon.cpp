299c6572ade9ee6647dbc8bd2ab35abb6a5e39f3
최영후 <dudgn6392@naver.com>
2023-06-07 20:43:41 +0900

    Add Weapon Class

---
+++ Source/Multiplay_TPS/Weapon/Weapon.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapon.h"
+#include "Components/SphereComponent.h"
+
+// Sets default values
+AWeapon::AWeapon()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
+
+	weaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("WeaponMesh"));
+	weaponMesh->SetupAttachment(RootComponent);
+	SetRootComponent(weaponMesh);
+
+	weaponMesh->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Block);
+	weaponMesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Ignore);
+	weaponMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	areaSphere = CreateDefaultSubobject<USphereComponent>(TEXT("AreaSphere"));
+	areaSphere->SetupAttachment(RootComponent);
+	areaSphere->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	areaSphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+// Called when the game starts or when spawned
+void AWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (HasAuthority())
+	{
+		areaSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+		areaSphere->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+	}
+}
+
+// Called every frame
+void AWeapon::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
